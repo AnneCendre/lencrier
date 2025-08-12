0f2cbcdc48bd8e53242ccedcbc78f1305fdbf92b
AnneCendre <anne.cendre.5@gmail.com>
2025-08-12 18:01:27 +0200

    lecture des 3 journaux et tri par ordre de création idLencrier

--- src/fusion.py
+++ src/fusion.py
@@ -15,6 +15,8 @@ class Article:
         return f"{self.idLencrier}({self.titre})"
 
     def __lt__ (self, other):
+        # le tri par idLencrier est un pis aller : certains articles retro-publiés n'apparaissent pas dans l'ordre
+        # TODO : la surcharge de l'opérateur "<" ne semble pas fonctionner, j'ai du utiliser sorted(res, key=attrgetter('idLencrier'))
         self.idLencrier < other.idLencrier
 
 def lireArticles(nomLencrier, fichierSource):
@@ -30,12 +32,16 @@ def lireArticles(nomLencrier, fichierSource):
     
 
 nomFichier = "./exports/2024-08-08_Cendre.html"
-nomLencrier = 'Cendre'
+nomLencrier = 'caillecendre'
 res = lireArticles(nomLencrier, nomFichier)
+nomFichier = "./exports/2024-08-08_Les amours de Cendre.html"
+res.extend(lireArticles("cendre", nomFichier))
+nomFichier = "./exports/2024-08-08_Mes écrits manuscrits.html"
+res.extend(lireArticles('manuscrits2caille', nomFichier))
 
 csv = open("./result/data.csv", "w")
 csv.write('nomLencrier;  id     ; dateLencrier              ; titre     ; length \n')
-for article in sorted(res, key=attrgetter('titre')):
+for article in sorted(res, key=attrgetter('idLencrier')):
     ligne =  '"' + article.nomLencrier + '" ; "' + article.idLencrier + '" ; "' + article.dateLencrier + '" ; "' + article.titre  + '" ; "' + str(len(article.content)) + '"'
     csv.write(ligne + '\n')
 
